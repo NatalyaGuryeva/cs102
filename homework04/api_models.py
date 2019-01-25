c01f4301c484c48b003a5cbfc1956ff744862290
NatalyaGuryeva <43902981+NatalyaGuryeva@users.noreply.github.com>
2019-01-26 02:43:10 +0300

    Create api_models.py

---
+++ homework04/api_models.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class BaseUser(BaseModel):
+    """ Модель пользователя с базовыми полями """
+    id: int
+    first_name: str
+    last_name: str
+    online: int
+    deactivated: Optional[str]
+
+
+class User(BaseUser):
+    """ Модель пользователя с необязательным полем дата рождения """
+    bdate: Optional[str]
+
+
+class Message(BaseModel):
+    """ Модель сообщения """
+    body: Optional[str]
+    date: int
+    from_id: int
+    id: int
+    out: Optional[int]
+    read_state: Optional[int]
+    user_id: int
